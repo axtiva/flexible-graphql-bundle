6657c63b61faa9c067fc27fdba8a73b71b788613
ec <bpteam22@gmail.com>
2024-05-28 21:40:18 +0200

    update symfony version in dependency

--- src/Command/GenerateDirectiveResolverCommand.php
+++ src/Command/GenerateDirectiveResolverCommand.php
@@ -7,12 +7,14 @@ namespace Axtiva\FlexibleGraphqlBundle\Command;
 use Axtiva\FlexibleGraphql\Builder\CodeGeneratorBuilderInterface;
 use Axtiva\FlexibleGraphql\Utils\SchemaBuilder;
 use GraphQL\Type\Definition\Directive;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(name: 'flexible_graphql:generate-directive-resolver')]
 class GenerateDirectiveResolverCommand extends Command
 {
     protected static $defaultName = 'flexible_graphql:generate-directive-resolver';
